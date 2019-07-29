829e807e1ed86a9df75a1d9d6b9b5d3bcfa0c7c7
oduk-sc <oduk@scand.com>
2019-07-29 11:25:55 +0300

    Updated to v.0.1.10

--- src/provider/test.js
+++ src/provider/test.js
@@ -1,3 +1,3 @@
 export const test = {
-  testCommand: 'mpx://fetchData?type=seasons&url=http://feed.entertainment.tv.theplatform.com/f/9_yWhC/aplcstr30-tv-seasons',
+  testCommand: 'mpx://fetchData?type=series&url=P2ZlZWRVcmw9aHR0cDovL2ZlZWQuZW50ZXJ0YWlubWVudC50di50aGVwbGF0Zm9ybS5jb20vZi85X3lXaEMvYXBsY3N0cjMwLXNlcmllcyZsaW1pdD0y',
 };
